6e7e2519b70f6afaf3d2a7669b593e9aaf2a4086
thecr7guy2 <saisofficialmail@gmail.com>
2025-07-06 21:10:19 +0000

    Added download from hf_hub code

--- blog/upload.py
+++ blog/upload.py
@@ -1,13 +1,13 @@
  #!/usr/bin/env python3
 
 import os
-from huggingface_hub import HfApi
+from huggingface_hub import HfApi,hf_hub_download
 
 # === Configuration ===
 # Your repo in the form "username/repo_name" (must already exist)
 REPO_ID = "thecr7guy/GPT2fromScratch"
 # Local path to the file you want to upload
-LOCAL_FILE_PATH = "checkpoint.pth"
+LOCAL_FILE_PATH = "../logs/."
 # Where the file will live in the repo (can include subfolders)
 PATH_IN_REPO = "checkpoints/cc_stories_epoch_9.pth"
 
@@ -18,11 +18,19 @@ if not TOKEN:
 
 # === Upload ===
 api = HfApi()
-api.upload_file(
-    path_or_fileobj=LOCAL_FILE_PATH,   # local file path
-    path_in_repo=PATH_IN_REPO,         # destination path in the repo
-    repo_id=REPO_ID,                   # your repo
-    token=TOKEN                        # your access token
+# api.upload_file(
+#     path_or_fileobj=LOCAL_FILE_PATH,   # local file path
+#     path_in_repo=PATH_IN_REPO,         # destination path in the repo
+#     repo_id=REPO_ID,                   # your repo
+#     token=TOKEN                        # your access token
+# )
+
+hf_hub_download(
+    repo_id=REPO_ID,
+    filename=PATH_IN_REPO,   # remote path
+    cache_dir=LOCAL_FILE_PATH,     # where to mirror the cache
+    token=TOKEN,             # can be None if public
+    local_dir=LOCAL_FILE_PATH,     # local copy root
 )
 
 print(f"✅ Uploaded {LOCAL_FILE_PATH} to {REPO_ID}/{PATH_IN_REPO}")
